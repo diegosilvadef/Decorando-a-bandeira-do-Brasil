627205b5ea1c326cddf5ce6626ad838a3aed2a4a
Diego Silva <diegodev246@gmail.com>
2020-10-31 18:03:39 -0400

    versao 1
    primieiro commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'app')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+app.listen(9000);
